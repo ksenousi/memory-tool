c37b07bd8902829211c122bc9205be4d5f8d8184
Karim Elsenousi <karimelsenousi@karims-air.home>
2019-07-29 22:27:43 +0100

    more changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import React, { useState } from "react";
 import "./App.css";
 import { ConfigForm, FormData } from "./config-form/config-form";
+import * as R from "ramda";
 
 enum Status {
   init = "init",
   mem = "mem",
   done = "done"
 }
+
+const setValue = (cb: any, index: number) => (
+  event: React.ChangeEvent<HTMLInputElement>
+) => {
+  const value = event.target.value;
+  cb((nums: number[]) => {
+    return R.adjust(index, () => (value ? Number(value) : 0), nums);
+  });
+};
 
 const genRandNums = (length: number, numDigits: number): number[] => {
   const min = Math.pow(10, numDigits - 1);
@@ -17,11 +27,13 @@ const genRandNums = (length: number, numDigits: number): number[] => {
 };
 
 const checkAnswer = (nums: number[], inputs: number[]) => () => {
-  console.log(nums)
+  const isCorrect: boolean = R.equals(nums, inputs);
+  console.log("isCorrect: ", isCorrect);
 };
 
 const App: React.FC = () => {
   const [nums, setNums] = useState<number[]>([]);
+  const [inputNums, setInputNums] = useState<number[]>([]);
   const [status, setStatus] = useState<Status>(Status.init);
 
   const onSubmit = ({ digits, length, duration }: FormData) => {
@@ -40,13 +52,22 @@ const App: React.FC = () => {
       <div>
         {status === Status.mem ? nums.map(num => <h2 key={num}>{num}</h2>) : ""}
         <div className="pure-form test-inputs">
-          {status === Status.done ? nums.map(num => <input key={Math.random()} />) : ""}
+          {status === Status.done
+            ? nums.map((_, index: number) => (
+                <input key={index} onChange={setValue(setInputNums, index)} />
+              ))
+            : ""}
         </div>
-        {
-          status === Status.done 
-          ? <button className="pure-button pure-button-primary" onClick={checkAnswer(nums,[])}>Check</button> 
-          : ""
-        }
+        {status === Status.done ? (
+          <button
+            className="pure-button pure-button-primary"
+            onClick={checkAnswer(nums, inputNums)}
+          >
+            Check
+          </button>
+        ) : (
+          ""
+        )}
       </div>
     </div>
   );
