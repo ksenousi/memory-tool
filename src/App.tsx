ce99758d452ca6f32026dbfed7d7efc4dbbfdf9b
karim <ksenousi@gmail.com>
2019-08-10 17:12:10 +0100

    changed to material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React, { useState } from "react";
-import "./App.css";
 import { ConfigForm, FormData } from "./config-form/config-form";
 import * as R from "ramda";
+import {
+  Theme,
+  createMuiTheme,
+  makeStyles,
+  createStyles
+} from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { TextField, Button } from "@material-ui/core";
 
 enum Status {
   init = "init",
   mem = "mem",
+  test = "test",
   done = "done"
 }
 
@@ -13,9 +22,7 @@ const setValue = (cb: any, index: number) => (
   event: React.ChangeEvent<HTMLInputElement>
 ) => {
   const value = event.target.value;
-  cb((nums: number[]) => {
-    return R.update(index, Number(value), nums);
-  });
+  cb((nums: number[]) => {nums[index] = Number(value); return nums});
 };
 
 const genRandNums = (length: number, numDigits: number): number[] => {
@@ -26,16 +33,19 @@ const genRandNums = (length: number, numDigits: number): number[] => {
   return Array.from({ length }, genRandNum);
 };
 
-const checkAnswer = (nums: number[], inputs: number[]) => () => {
-  const isCorrect: boolean = R.equals(nums, inputs);
-  console.log("nums: ", nums);
-  console.log("isCorrect: ", isCorrect);
-};
-
 const App: React.FC = () => {
   const [nums, setNums] = useState<number[]>([]);
   const [inputNums, setInputNums] = useState<number[]>([]);
   const [status, setStatus] = useState<Status>(Status.init);
+  const [isCorrectAnswer, setIsCorrectAnswer] = useState(false);
+
+  const theme: Theme = createMuiTheme({
+    palette: {
+      type: "dark"
+    }
+  });
+
+  const classes = useStyles(theme);
 
   const onSubmit = ({ digits, length, duration }: FormData) => {
     const randNums: number[] = genRandNums(length, digits);
@@ -43,35 +53,81 @@ const App: React.FC = () => {
     setStatus(Status.mem);
 
     setTimeout(() => {
-      setStatus(Status.done);
+      setStatus(Status.test);
     }, duration * 1000);
   };
 
+  const checkAnswer = (nums: number[], inputs: number[]) => () => {
+    console.log("yo", nums, inputs);
+    const isCorrect: boolean = R.equals(nums, inputs);
+    setIsCorrectAnswer(isCorrect);
+    setStatus(Status.done);
+  };
+
   return (
-    <div className="App">
-      <ConfigForm onSubmit={onSubmit} />
-      <div>
-        {status === Status.mem ? nums.map(num => <h2 key={num}>{num}</h2>) : ""}
-        <div className="pure-form test-inputs">
-          {status === Status.done
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className={classes.app}>
+        <ConfigForm onSubmit={onSubmit} />
+        <div className={classes.content}>
+          {status === Status.mem
+            ? nums.map(num => <h2 key={num}>{num}</h2>)
+            : ""}
+          {status === Status.test
             ? nums.map((_, index: number) => (
-                <input key={index} onChange={setValue(setInputNums, index)} />
+                <TextField
+                  className={classes.textField}
+                  key={index}
+                  onChange={setValue(setInputNums, index)}
+                />
               ))
             : ""}
+          {status === Status.test ? (
+            <Button
+              className={classes.checkButton}
+              color="secondary"
+              variant="contained"
+              onClick={checkAnswer(nums, inputNums)}
+            >
+              Check
+            </Button>
+          ) : (
+            ""
+          )}
+          {status === Status.done ? (
+            <h1>{isCorrectAnswer ? "Correct" : "Incorrect"}</h1>
+          ) : (
+            ""
+          )}
         </div>
-        {status === Status.done ? (
-          <button
-            className="pure-button pure-button-primary"
-            onClick={checkAnswer(nums, inputNums)}
-          >
-            Check
-          </button>
-        ) : (
-          ""
-        )}
       </div>
-    </div>
+    </ThemeProvider>
   );
 };
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    app: {
+      marginTop: "20px",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center"
+    },
+    content: {
+      marginTop: "20px",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center"
+    },
+    textField: {
+      marginTop: theme.spacing(1),
+      marginBottom: theme.spacing(1),
+      width: 200
+    },
+    checkButton: {
+      marginTop: "20px"
+    }
+  })
+);
 
 export default App;
--- src/config-form/config-form.tsx
+++ src/config-form/config-form.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./config-form.css";
+import { TextField, Button, makeStyles, Theme, createStyles } from "@material-ui/core";
 
 interface Props {
   onSubmit: (formData: FormData) => void;
@@ -15,46 +15,69 @@ const setValue = (cb: (value: number) => void) => (
   event: React.ChangeEvent<HTMLInputElement>
 ) => cb(Number(event.target.value));
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    textField: {
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+      width: 200,
+    },
+    dense: {
+      marginTop: 19,
+    },
+    menu: {
+      width: 200,
+    },
+  }),
+);
+
 export const ConfigForm = (props: Props) => {
+  const classes = useStyles();
+
   const [duration, setDuration] = useState(3);
   const [digits, setDigits] = useState(3);
   const [length, setLength] = useState(3);
 
   return (
-    <div className="pure-form">
-      <label htmlFor="duration">Duration</label>
-      <input
+    <div className={classes.container} >
+      <TextField
         id="duration"
-        placeholder="duration"
+        label="Duration"
         type="text"
+        className={classes.textField}
         value={duration}
         onChange={setValue(setDuration)}
       />
 
-      <label htmlFor="digits">Digits</label>
-      <input
+      <TextField
         id="digits"
-        placeholder="digits"
+        label="Digits"
         type="text"
+        className={classes.textField}
         value={digits}
         onChange={setValue(setDigits)}
       />
 
-      <label htmlFor="reps">Reps</label>
-      <input
+      <TextField
         id="reps"
-        placeholder="reps"
+        label="Reps"
         type="text"
+        className={classes.textField}
         value={length}
         onChange={setValue(setLength)}
       />
 
-      <button
-        className="pure-button pure-button-primary"
+      <Button
+        color="primary"
+        variant="contained"
         onClick={() => props.onSubmit({ duration, digits, length })}
       >
         Go
-      </button>
+      </Button>
     </div>
   );
 };
